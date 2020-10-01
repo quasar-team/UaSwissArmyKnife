9869d0240e4f4333ace6f49103be45f553ff0fc2
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2020-10-01 17:18:44 +0200

    WIP with the browser

--- src/uasak_browse.cpp
+++ src/uasak_browse.cpp
@@ -2,6 +2,13 @@
 
 #include <boost/program_options.hpp>
 
+#include <LogIt.h>
+
+/* From open62541-compat or UA-SDK */
+#include <uaclient/uaclientsdk.h>
+
+using namespace UaClientSdk;
+
 struct Options
 {
     std::string endpoint_url ;
@@ -36,8 +43,35 @@ Options parse_program_options(int argc, char* argv[])
 
 }
 
+void browse_recurse(UaNodeId n0)
+{
+  
+}
+
 int main (int argc, char* argv[])
 {
+  Log::initializeLogging();
+
   Options options (parse_program_options(argc, argv));
   std::cout << "will connect to the following endpoint: " << options.endpoint_url << std::endl;
+
+  SessionConnectInfo      connectInfo;
+  SessionSecurityInfo     securityInfo;
+
+  UaSession session;
+  UaStatus status = session.connect(
+    options.endpoint_url.c_str(),
+    connectInfo,
+    securityInfo,
+    nullptr /*callback*/);
+  if (!status.isGood())
+  {
+    LOG(Log::ERR) << status.toString().toUtf8();
+    return 1;
+  }
+
+  LOG(Log::INF) << "Session opened ;-)";
+
+
+
 }
