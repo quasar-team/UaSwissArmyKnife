d9cdeb88375d32bbadcd683393a91327463ac9cf
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2020-11-19 14:59:41 +0100

    Using prepare ecosystem from github

--- build_linux_open62541.sh
+++ build_linux_open62541.sh
@@ -1,6 +1,6 @@
 rm -Rf build
 mkdir build && cd build
-git clone ssh://git@gitlab.cern.ch:7999/atlas-dcs-opcua-servers/PrepareEcosystem.git
+git clone git@github.com:pnikiel/PrepareEcosystem.git
 sh PrepareEcosystem/prepare.sh --with_LogIt --with_o6compat --tag_o6compat v1.3.5
 cmake ../
 make
