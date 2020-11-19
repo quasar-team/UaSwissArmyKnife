2718af675aa9d9abc5c6879a8368f63219cecce4
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2020-11-19 17:55:42 +0100

    Using newer open62541-compat for missing node classes

--- build_linux_open62541.sh
+++ build_linux_open62541.sh
@@ -1,6 +1,6 @@
 rm -Rf build
 mkdir build && cd build
 git clone https://github.com/pnikiel/PrepareEcosystem.git
-bash PrepareEcosystem/prepare.sh --with_LogIt --with_o6compat --tag_o6compat v1.3.5
+bash PrepareEcosystem/prepare.sh --with_LogIt --with_o6compat --tag_o6compat OPCUA-2079_support_full_nodeclasses 
 cmake ../
 make
