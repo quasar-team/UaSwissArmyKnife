150d69ed8885bcb3d7ef99d782aa5eb57196710c
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2020-11-19 15:06:30 +0100

    using https to get from github

--- build_linux_open62541.sh
+++ build_linux_open62541.sh
@@ -1,6 +1,6 @@
 rm -Rf build
 mkdir build && cd build
-git clone git@github.com:pnikiel/PrepareEcosystem.git
+git clone https://github.com/pnikiel/PrepareEcosystem.git
 sh PrepareEcosystem/prepare.sh --with_LogIt --with_o6compat --tag_o6compat v1.3.5
 cmake ../
 make
