3564714a82adbec1cfe3cbc34d2338217695bc64
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2020-11-19 15:12:22 +0100

    Forcing bash in the build script

--- build_linux_open62541.sh
+++ build_linux_open62541.sh
@@ -1,6 +1,6 @@
 rm -Rf build
 mkdir build && cd build
 git clone https://github.com/pnikiel/PrepareEcosystem.git
-sh PrepareEcosystem/prepare.sh --with_LogIt --with_o6compat --tag_o6compat v1.3.5
+bash PrepareEcosystem/prepare.sh --with_LogIt --with_o6compat --tag_o6compat v1.3.5
 cmake ../
 make
